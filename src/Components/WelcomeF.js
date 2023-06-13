005af3d5b73bdd46b5cd7bfed87fedc93e2bf662
Lama kharaz <kharrazlama@gmail.com>
2023-06-13 18:50:37 +0300

    src/Components/WelcomeF.js

--- src/Components/WelcomeF.js
+++ src/Components/WelcomeF.js
@@ -4,6 +4,7 @@ function WelcomeF(props) {
 
     //hooks
     const[counter,setCounter]=useState(0);
+    const[name,setName]=useState(" ");
 
     return (<>
     <h6>(Function)</h6>
@@ -11,7 +12,9 @@ function WelcomeF(props) {
     <h6> type, {props.gender}! </h6>
     <h6> Age:{props.age}</h6>
     <h6> counter: {counter}</h6>
+    <h6> Name: {name}</h6> 
     <button onClick={()=>setCounter(counter+1)}>Counte+1 </button>
+    <button onClick={()=>setName(props.name)}>Change Name </button>
     
     </>)
 }export default WelcomeF;
